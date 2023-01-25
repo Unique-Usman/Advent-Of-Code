aa926ffcbde2b680c01d2e21895917ffad0440c0
Unique-Usman <usmanakinyemi202@gmail.com>
2023-01-26 02:42:05 +0530

    The answer I got from the previous code was not correct so, I updated… … the code

--- Advent-1-2022/advent12022.py
+++ Advent-1-2022/advent12022.py
@@ -4,16 +4,22 @@ import os
 def findMaxElves(file):
     f=open(file, "r")
     elvesList = []
-    elvelist = []
+    elvelist = []  
+
+    #reading the line and appending the value in intger form to a elvelist, if the value is "" the elveslist will append to the bigger list elvesList and empties
     for line in f.readlines():
         if line.strip() == "":
             elvesList.append(elvelist)
             elvelist = []
         else:
             elvelist.append(int(line.strip()))
+
+    #summing each value in each sub list and update the maxELvesValue to list of the individual sum of it sublist.
+    for index,elve in enumerate(elvesList):
+        elvesList[index] = sum(elve)
+        
     maxElvesValue = max(elvesList)
-    maxELvesPos = elvesList.index(maxElvesValue) + 1
-    return(maxElvesValue[0], maxELvesPos)
+    return(maxElvesValue)
 
 def main():
     file = "file.txt"
